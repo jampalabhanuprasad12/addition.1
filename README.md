4246e1413f806d1a04bc8ff49d314cf5228c90d8
jampalabhanuprasad12 <jampalabhanprasad12@gmial.com>
2026-08-24 20:57:43 +0530

    added a python code to add a 2 numbers

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 addition of 2 numbers.
 this is written in the python  language.
 this code is used to add the 2 numbers.
+this code has total 5 lines.
---
+++ addition.py
@@ -0,0 +1,5 @@
+a= int(input("Enter your first number:"))
+b=int(input("Enter your second number:"))
+c=a+b
+print(f"the addition of {a} + {b} = {c} ")
+print("Addition completed successfully.")
